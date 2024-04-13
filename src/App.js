2302671b103ce790f05ab7c8e24e79411f8c5a1e
stkener <stkener@gmail.com>
2024-04-13 12:42:28 -0300

    portfolio2024

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+import Home from './component/Home';
+import Barra from "./component/Barra";
+import About from "./component/About";
+import Proyectos from "./component/Proyectos";
+import Experiencia from "./component/Experiencia";
+import Educacion from "./component/Educacion";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Proyectos" element={<Proyectos />} />
+          <Route path="/Experiencia" element={<Experiencia />} />
+          <Route path="/Educacion" element={<Educacion />} />
+        </Routes>
+      </div>
+    </Router> 
   );
 }
 
 export default App;
+
+/*
+<Route path="/Proyectos" element={<Proyectos />} />
+<Route path="/Experiencia" element={<Experiencia />} />
+<Route path="/Educacion" element={<Educacion />} />
+
+<Barra />
+*/
